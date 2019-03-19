fd41d693ac66711e837509fa687a96080ae330b7
Lonneke Dikmans <lonneke.dikmans@eproseed.com>
2019-03-19 21:26:11 +0100

    removed some logging to clarify the flow

--- src/js/appController.js
+++ src/js/appController.js
@@ -45,20 +45,16 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojrouter', 'o
                 var smQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
                 self.smScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
 
-                // Header
-                // Application Name used in Branding Area
-                self.appName = ko.observable("Customers Profile");
-
-                // User Info used in Global Navigation area
-                self.userLogin = ko.observable("Not yet logged in");
+                
                 self.userLoggedIn = ko.observable(JSON.parse(sessionStorage.getItem('userLoggedIn')));
 
                 self.doLogin = function (customer) {
+                    console.log('logging in');
                     self.globalContext.customer = customer;
-                    self.userLogin(self.globalContext.customer.title + " " + self.globalContext.customer.firstName + " " + self.globalContext.customer.lastName);
                     self.userLoggedIn("Y");
                     sessionStorage.setItem('userLoggedIn', true);
                     sessionStorage.setItem('profileId', customer._id);
+                    sessionStorage.setItem('signUp', false);
                     self.globalContext.username = customer.email;
                     self.globalContext.userLoggedIn = "Y";
                     var signinEvent = {
@@ -73,11 +69,6 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojrouter', 'o
 
                 };
 
-
-
-                // this function will communicate an event with the parent window
-                // typically used for applications that run inside an IFRAME to inform the
-                // embedding application about what is going on.      
                 self.callParent = function (message) {
                     console.log('send message from Customers to parent window');
                     // here we can restrict which parent page can receive our message
@@ -99,10 +90,8 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojrouter', 'o
                     console.log('in init appcontroler');
                     var username = self.globalContext.userName;
                     console.log('self.globalConext.userName = ' + self.globalContext.userName);
-                    console.log('essionStorage.userLoggedIn: ' + JSON.parse(sessionStorage.getItem('userLoggedIn')));
+                    console.log('sessionStorage.userLoggedIn: ' + JSON.parse(sessionStorage.getItem('userLoggedIn')));
                     window.addEventListener("message", function (event) {
-                        console.log("Received message from embedding application " + event);
-                        console.log("Payload =  " + JSON.stringify(event.data));
                         if (event.data.eventType === "globalContext") {
                             self.globalContext = event.data.payload.globalContext;
                             if (self.globalContext.customer) {
@@ -128,7 +117,6 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojrouter', 'o
                                         }
                                 );
                             }
-                            //inform listeners of new global context
                             self.globalContextListeners.forEach(function (listener) {
                                 listener(self.globalContext);
                             });
